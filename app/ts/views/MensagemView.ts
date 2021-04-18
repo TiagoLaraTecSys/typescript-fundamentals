5a4618d5e3f13868a29e8e2774eaceac4f6a2039
Tiago de Lara Ribeiro <tribeiro@1worldsync.com>
2021-04-18 20:53:14 -0300

    Transformando metodo template da classe view em Abstrata para obrigar quem for extender ela a implementar o metodo template

--- app/ts/views/MensagemView.ts
+++ app/ts/views/MensagemView.ts
@@ -1,8 +1,9 @@
 class MensagemView extends View<string> {
-
+    
     template(model: string): string {
         return `
             <p class="alert alert-info">${model}</p>
         `;
     }
+
 }
