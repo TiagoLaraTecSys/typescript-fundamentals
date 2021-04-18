6aff442868da07fa71379264b953e3647a4fe1ee
Tiago de Lara Ribeiro <tribeiro@1worldsync.com>
2021-04-18 20:44:18 -0300

    Polimorfismo com Type script para as classes de view que heram VIew

--- app/js/views/MensagemView.js
+++ app/js/views/MensagemView.js
@@ -1,10 +1,4 @@
-class MensagemView {
-    constructor(select) {
-        this._elemento = document.querySelector(select);
-    }
-    update(model) {
-        this._elemento.innerHTML = this.template(model);
-    }
+class MensagemView extends View {
     template(model) {
         return `
             <p class="alert alert-info">${model}</p>
--- app/ts/views/MensagemView.ts
+++ app/ts/views/MensagemView.ts
@@ -1,15 +1,5 @@
-class MensagemView {
+class MensagemView extends View<string> {
 
-    private _elemento: Element;
-
-    constructor(select: string){
-
-        this._elemento = document.querySelector(select);
-    }
-
-    update(model: string) {
-        this._elemento.innerHTML = this.template(model);
-    }
     template(model: string): string {
         return `
             <p class="alert alert-info">${model}</p>
